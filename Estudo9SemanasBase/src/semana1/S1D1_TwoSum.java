c5ca1b25a7502042765c96abba548627618a6c03
Joao Leonardo Macanhao <joao.macanhao@melhorescolha.net>
2025-12-03 20:55:53 -0400

    s1d1

--- Estudo9SemanasBase/src/semana1/S1D1_TwoSum.java
+++ Estudo9SemanasBase/src/semana1/S1D1_TwoSum.java
@@ -22,3 +22,19 @@ public class S1D1_TwoSum {
     }
 
 }
+
+
+/*
+ * Este código utiliza a estratégia de tabela de dispersão (HashMap)
+ * para encontrar, em tempo linear, dois valores em um array cuja soma
+ * resulta no alvo especificado. Durante a iteração sobre o array, cada elemento 
+ * é analisado para verificar se o valor complementar necessário para atingir o alvo já foi
+ * encontrado anteriormente. Caso o complemento esteja armazenado no mapa, a solução é
+ * imediatamenteretornada com os índices correspondentes. Caso contrário, o valor atual é 
+ * nserido na estrutura para consultas futuras. Esse mecanismo evita buscas repetitivas e
+ * elimina a necessidade de loops aninhados, reduzindo a complexidade da solução para O(N). 
+ * Trata-se, portanto, de uma abordagem eficiente, direta e amplamente utilizada para 
+ * resolver o problema de Two Sum de forma otimizada.
+ * 
+ * */
+ */
